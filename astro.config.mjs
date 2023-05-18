b82456c5e1455f0a52c5d488136ed08d5eb285b7
Anuwong Mongkudkornburee <anuwong.mon@truedigital.com>
2023-05-18 09:31:39 +0700

    feat: add unsplash image render

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import image from "@astrojs/image";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    tailwind(),
+  ],
+});
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
-/// <reference types="astro/client" />
+/// <reference types="@astrojs/image/client" />
+
+interface ImportMetaEnv {
+  readonly UNSPLASH_ACCESS_KEY: string;
+  readonly UNSPLASH_SECRET_KEY: string;
+  readonly PUBLIC_UNSPLASH_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
