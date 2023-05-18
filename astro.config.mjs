a5a970186dfd3058ff2f3f5f842ef3044f69ae10
Anuwong Mongkudkornburee <anuwong.mon@truedigital.com>
2023-05-18 18:50:51 +0700

    feat: initial firebase hosting config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
+  output: "static",
   integrations: [
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
